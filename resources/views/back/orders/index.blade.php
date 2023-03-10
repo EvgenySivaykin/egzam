6f4d26ba2388d1941a8eb98aaa4a5e9d2811fadd
EvgenySivaykin <114056472+EvgenySivaykin@users.noreply.github.com>
2023-03-10 10:40:07 +0200

    second change afer

--- resources/views/back/orders/index.blade.php
+++ resources/views/back/orders/index.blade.php
@@ -16,8 +16,9 @@
 
 
                                 <div class="list-table__content">
-                                    <h3>#{{$order->id}}</h3>
-                                    <b class="m-5">{{$order->user->name}}</b>
+                                    <h3>#{{$order->id}}
+                                        <b class="m-5">{{$order->user->name}}</b>
+                                    </h3>
                                     <ul class="list-group">
 
                                         @foreach($order->dishes->dishes as $dish)
@@ -28,10 +29,21 @@
                                     </ul>
                                 </div>
 
-                                <div class="list-table__buttons">
-
-
+                                <div>
+                                    @if($order->status == 0)
+                                    <form action="{{route('orders-update', $order)}}" method="post" class="mt-2">
+                                        <button type="submit" class="btn btn-outline-primary">Finish Order</button>
+                                        @csrf
+                                        @method('put')
+                                    </form>
+                                    @endif
+                                    <form action="{{route('orders-delete', $order)}}" method="post" class="mt-2">
+                                        <button type="submit" class="btn btn-outline-danger">Delete Order</button>
+                                        @csrf
+                                        @method('delete')
+                                    </form>
                                 </div>
+
                             </div>
                         </li>
 
---
+++ resources/views/emails/shipped.blade.php
@@ -0,0 +1,8 @@
+<x-mail::message>
+    # Order Shipped
+
+    Your order Number {{$ID}} has been confirmed!
+
+    Thanks,<br>
+    Restaurants Team
+</x-mail::message>
