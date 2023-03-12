78212c33bfbfe42535ca9f070a6c0e02338b8379
EvgenySivaykin <114056472+EvgenySivaykin@users.noreply.github.com>
2023-03-12 09:20:00 +0200

    photo logo

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -23,7 +23,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{-- {{ config('app.name', 'Laravel') }} --}}
-                    <img class="logo" src="{{asset('img/logo.jpg')}}" alt="logo">
+                    <img class="logo" src="{{asset('img/logo.jpeg')}}" alt="logo">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
